b67f1b4ff552da4e71082eb19f5cf8e33f143ec7
Lucas <lucas.labbe@etu.univ-lyon1.fr>
2025-10-28 22:26:38 +0100

    Test de l'API

--- frontend/app.py
+++ frontend/app.py
@@ -1,5 +1,17 @@
 import pandas as pd
 import streamlit as st
+import requests
 
 st.set_page_config(page_title="Risques d'accidents", layout="wide")
-st.title("BONJOUR A TOUS")
+
+st.subheader("Prédiction du risque d'accident selon les caractéristiques des routes")
+
+API_URL = "https://risk-accident-prediction.onrender.com"
+
+# Ajouter un score
+requests.post(f"{API_URL}/add_score", json={"username": "Sara", "score": 12})
+
+# Récupérer le leaderboard
+res = requests.get(f"{API_URL}/leaderboard")
+leaderboard = res.json()
+st.write(leaderboard)
