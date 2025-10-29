088cee0053e67db1c2b9f23a0e1988a51f56a165
Lucas <lucas.labbe@etu.univ-lyon1.fr>
2025-10-29 14:42:52 +0100

    Changements pour importations des modules

--- frontend/app.py
+++ frontend/app.py
@@ -5,8 +5,8 @@ import joblib
 import numpy as np
 import random
 import loguru as logger
-from frontend.pages.jeu import lancer_jeu
-from frontend.pages.accueil import accueil
+from sections import jeu
+from sections import accueil
 
 
 st.set_page_config(page_title="Risques d'accidents", layout="wide", page_icon="🚗")
@@ -16,6 +16,6 @@ st.sidebar.title("Navigation")
 pages=st.sidebar.radio("Sélectionnez une page",["Accueil", "Explorations des données", "Prédiction des accidents", "Jeu des prédictions", "A propos de l'application"])
 
 if pages == "Accueil":
-    accueil()
+    accueil.lancer_accueil()
 if pages == "Jeu des prédictions":
-    lancer_jeu()
+    jeu.lancer_jeu()
